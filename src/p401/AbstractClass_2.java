884ec61ddb1df6bdf7a387b7291f342098f1b9f3
AshShine <protoss1207@gmail.com>
2022-03-26 23:42:00 +0900

    Upload java self-study

---
+++ src/p401/AbstractClass_2.java
@@ -0,0 +1,22 @@
+package p401;
+
+abstract class A{
+    abstract void abc();
+}
+
+public class AbstractClass_2 {
+    public static void main(String[] args) {
+        A a1 = new A(){
+            void abc(){
+                System.out.println("방법2. 익명 이너 클래스 방법으로 객체 생성");
+            }
+        };
+        A a2 = new A(){
+            void abc(){
+                System.out.println("방법2. 익명 이너 클래스 방법으로 객체 생성");
+            }
+        };
+        a1.abc();
+        a2.abc();
+    }
+}
