33c1f01a9ed6a6bc482a049356fa93a9078f4f04
White-Asher <protoss1207@gmail.com>
2022-04-10 23:55:26 +0900

    ...

---
+++ java_playground/src/calendar/ScheduleRegistration.java
@@ -0,0 +1,15 @@
+package calendar;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ScheduleRegistration {
+    HashMap<Date, Plan> planSave;
+    public registrateDate(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println();
+        planSave = new HashMap<Date, Plan>();
+
+    }
+}
